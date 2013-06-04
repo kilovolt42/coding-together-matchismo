e1df79bb3e8fdcdd9ce737b2740aef47ae97f8fa
Kyle Stevens <kvstevns@gmail.com>
2013-06-04 17:37:13 -0400

    Adds the PlayingCard, Deck, and PlayingCardDeck classes from the second lecture.

---
+++ Matchismo/Model/Deck.h
@@ -0,0 +1,17 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Kyle Stevens on 6/4/13.
+//  Copyright (c) 2013 kilovolt42. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+
+@interface Deck : NSObject
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop;
+- (Card *)drawRandomCard;
+
+@end
---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Kyle Stevens on 6/4/13.
+//  Copyright (c) 2013 kilovolt42. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
