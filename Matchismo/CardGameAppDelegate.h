e6e65ddd98eac86078e51ffb4b8868d487cfbcaf
Kyle Stevens <kvstevns@gmail.com>
2013-06-04 16:22:42 -0400

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Kyle Stevens on 6/4/13.
+//  Copyright (c) 2013 kilovolt42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Kyle Stevens on 6/4/13.
+//  Copyright (c) 2013 kilovolt42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
